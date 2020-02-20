6e61aee64793be23ad8ce2fcc7e86afbd4faa5eb
Diego Rocchini <roquinni@gmail.com>
2020-02-20 20:39:10 -0300

    Added Builder

--- test/TDDxUnitCore.Domain.Test/Courses/TestCourse.cs
+++ test/TDDxUnitCore.Domain.Test/Courses/TestCourse.cs
@@ -2,23 +2,35 @@
 using System;
 using Xunit;
 using TDDxUnitCore.Domain.Test._Tooling;
+using Xunit.Abstractions;
+using TDDxUnitCore.Domain.Test._Builders;
 
 namespace TDDxUnitCore.Domain.Test.Courses
 {
     public class TestCourse
     {
+        private readonly ITestOutputHelper _output;
+
+        public TestCourse(ITestOutputHelper output)
+        {
+            _output = output;
+            _output.WriteLine("Constructor called");
+        }
+
         [Fact(DisplayName = "MustCreateCourse")]
         public void MustCreateCourse()
         {
             var expectedCourse = new
             {
                 Name = "Dev",
+                Description = "My Desc",
                 Workload = (double)15.57,
                 Audience = Audience.Studant,
                 Cost = (double)1500
             };
 
-            var course = new Course(expectedCourse.Name, expectedCourse.Workload, expectedCourse.Audience, expectedCourse.Cost);
+            var course = new Course(
+                expectedCourse.Name, expectedCourse.Description, expectedCourse.Workload, expectedCourse.Audience, expectedCourse.Cost);
 
             expectedCourse.ToExpectedObject().ShouldMatch(course);
         }
@@ -28,15 +40,7 @@ namespace TDDxUnitCore.Domain.Test.Courses
         [InlineData(null)]
         public void MustHaveValidName(string invalidName)
         {
-            var expectedCourse = new
-            {
-                Name = "Dev",
-                Workload = (double)15.57,
-                Audience = Audience.Studant,
-                Cost = (double)1500
-            };
-
-            Assert.Throws<ArgumentException>(() => new Course(invalidName, expectedCourse.Workload, expectedCourse.Audience, expectedCourse.Cost))
+            Assert.Throws<ArgumentException>(() => BuilderCourse.New().WithName(invalidName).Build())
                 .WithMessage("Enter a valid name (not empty or null)");
         }
 
@@ -46,15 +50,7 @@ namespace TDDxUnitCore.Domain.Test.Courses
         [InlineData(-299.50)]
         public void MustHavePositiveHours(double invalidWorkload)
         {
-            var expectedCourse = new
-            {
-                Name = "Dev",
-                Workload = (double)15.57,
-                Audience = Audience.Studant,
-                Cost = (double)1500
-            };
-
-            Assert.Throws<ArgumentException>(() => new Course(expectedCourse.Name, invalidWorkload, expectedCourse.Audience, expectedCourse.Cost))
+            Assert.Throws<ArgumentException>(() => BuilderCourse.New().WithWorkload(invalidWorkload).Build())
                 .WithMessage("Enter a valid workload (greater than zero)");
         }
 
@@ -65,26 +61,19 @@ namespace TDDxUnitCore.Domain.Test.Courses
         [InlineData(-299.50)]
         public void MustHavePositiveCost(double invalidCost)
         {
-            var expectedCourse = new
-            {
-                Name = "Dev",
-                Workload = (double)15.57,
-                Audience = Audience.Studant,
-                Cost = (double)1500
-            };
-
-            Assert.Throws<ArgumentException>(() => new Course(expectedCourse.Name, expectedCourse.Workload, expectedCourse.Audience, invalidCost))
+            Assert.Throws<ArgumentException>(() => BuilderCourse.New().WithCost(invalidCost).Build())
                 .WithMessage("Enter a valid cost (greater than zero)");
         }
     }
 
 
 
-    class Course
+    public class Course
     {
-        public Course(string name, double workload, Audience audience, double cost)
+        public Course(string name, string description, double workload, Audience audience, double cost)
         {
             Name = name;
+            Description = description;
             Workload = workload;
             Audience = audience;
             Cost = cost;
@@ -94,7 +83,7 @@ namespace TDDxUnitCore.Domain.Test.Courses
 
         private void Validate()
         {
-            if (string.IsNullOrEmpty(Name))
+            if(string.IsNullOrEmpty(Name))
                 throw new ArgumentException("Enter a valid name (not empty or null)");
 
             if (Workload <= 0)
@@ -105,6 +94,7 @@ namespace TDDxUnitCore.Domain.Test.Courses
         }
 
         public string Name { get; private set; }
+        public string Description { get; private set; }
         public double Workload { get; private set; }
         public Audience Audience { get; private set; }
         public double Cost { get; private set; }
---
+++ test/TDDxUnitCore.Domain.Test/_Builders/BuilderCourse.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using TDDxUnitCore.Domain.Test.Courses;
+
+namespace TDDxUnitCore.Domain.Test._Builders
+{
+    public class BuilderCourse
+    {
+        private string _name = "Dev";
+        private string _description = "Dev for everyone";
+        private double _workload = 213.5;
+        private Audience _audience = Audience.NonDev;
+        private double _cost = 332;
+
+        public static BuilderCourse New()
+        {
+            return new BuilderCourse();
+        }
+
+
+        public BuilderCourse WithName (string name)
+        {
+            _name = name;
+            return this;
+        }
+        public BuilderCourse WithDescription(string description)
+        {
+            _description = description;
+            return this;
+        }
+        
+        public BuilderCourse WithWorkload(double workload)
+        {
+            _workload = workload;
+            return this;
+        }
+
+        public BuilderCourse WithCost(double cost)
+        {
+            _cost = cost;
+            return this;
+        }
+
+        public BuilderCourse WithAudience(Audience audience)
+        {
+            _audience = audience;
+            return this;
+        }
+
+        public Course Build()
+        {
+            return new Course(_name, _description, _workload, _audience, _cost);
+        }
+    }
+}
