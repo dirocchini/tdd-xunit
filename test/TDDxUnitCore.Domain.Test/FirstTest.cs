ee232508cae013ff406b566584de6aa3d2d1d9c0
Diego Rocchini <roquinni@gmail.com>
2020-02-19 23:03:40 -0300

    First Test

---
+++ test/TDDxUnitCore.Domain.Test/FirstTest.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TDDxUnitCore.Domain.Test
+{
+    public class FirstTest
+    {
+        [Fact(DisplayName = "Test")]
+        public void Test()
+        {
+            var a = 1;
+            var b = 1;
+
+            Assert.Equal(a, b);
+        }
+    }
+}
