24c304109e213d741ffe0c1d02572c835b0bb199
Diego Rocchini <roquinni@gmail.com>
2020-03-03 19:04:19 -0300

    Added Audience Validation For StudentMapping

--- src/TDDxUnitCore.CrossCutting/Mapper/StudentMappingProfile.cs
+++ src/TDDxUnitCore.CrossCutting/Mapper/StudentMappingProfile.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 using AutoMapper;
+using TDDxUnitCore.Domain._Base;
 using TDDxUnitCore.Domain.Audiences;
 using TDDxUnitCore.Domain.Students;
 
@@ -21,8 +22,14 @@ namespace TDDxUnitCore.CrossCutting.Mapper
             CreateMap<StudentDTO, Student>()
                 .ForMember(
                     dest => dest.Audience
-                    , opt => opt.MapFrom(p =>Audience.CTO)
+                    , opt => opt.MapFrom(p => ParseEnum(p.Audience))
                 );
+        }
+
+        private Audience ParseEnum(string audience)
+        {
+            RulerValidator.New().When(!Enum.TryParse<Audience>(audience, true, out var audienceConverted), Resources.InvalidAudience).ThrowException();
+            return audienceConverted;
         }
     }
 }
